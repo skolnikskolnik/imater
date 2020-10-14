0cb66f16d07227aa6c795f173f48a888002c1534
Julie Altman <jtskolnik@gmail.com>
2020-10-13 21:54:38 -0400

    separated js file, fixed issue of replacing items in local storage

---
+++ index.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    var date = new Date();
+    var dateOfMonth = date.getDate();
+    //Stores the month as a number (subtract one since it starts at zero)
+    var month = date.getMonth();
+    var hourOfDay = date.getHours();
+    var dayOfWeek = date.getDay();
+    var year = date.getFullYear();
+
+    //Display day of week, month, and year to the screen
+    var all_users = [];
+    var dateDisplay = $("#currentDay");
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    dateDisplay.text(daysOfWeek[dayOfWeek] + ", " + months[month] + " " + dateOfMonth + ", " + year);
+
+    //Have three modes, past, present, and future
+    var hourClass = $(".description");
+
+    //Check for existing local storage items
+    displayUserInput();
+    //Three types of states: past, present, and future
+
+    //For past, this occurs when Math.floor(current time) is less than the timeNumber
+    var currentTimeFloor = Math.floor(hourOfDay);
+
+    for (var i = 0; i < hourClass.length; i++) {
+        var hourNumber = i + 9;
+        var hourClassIndividual = hourClass[i];
+
+        if (currentTimeFloor > hourNumber) {
+            hourClassIndividual.classList.add("past");
+        }
+        else if (currentTimeFloor == hourNumber) {
+            hourClassIndividual.classList.add("present");
+        }
+        else {
+            hourClassIndividual.classList.add("future");
+        }
+    }
+
+    //Putting an event listener on the submit buttons that will save the text entered and display it permanently
+    var submitBtns = $(".saveBtn");
+
+    submitBtns.on("click", function () {
+        //Add a value to each element 
+        for (var i = 0; i < submitBtns.length; i++) {
+            submitBtns[i].setAttribute("index", i);
+            hourClass[i].setAttribute("index", i);
+        }
+
+        //Get the index of the button that was clicked
+        var thisIndex = ($(this).attr("index"));
+
+        //Now to get and store the input
+        var userInput = hourClass[thisIndex].value;
+
+        //The index is thisIndex and the value is userInput
+        event.preventDefault();
+
+        // create user object from submission
+        var user = {
+            index: thisIndex,
+            input: userInput,
+        };
+
+        // validate the fields
+
+        //all_users.push(user)
+        // set new submission
+        localStorage.setItem(thisIndex, userInput);
+    });
+
+
+    function displayUserInput() {
+        //Need to get the items from local storage
+        
+    }
+});
